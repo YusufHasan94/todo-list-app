bbdb33116d7f8a5debcbc36f058f66222d7dfa4a
YusufHasan94 <yhpolok@gmail.com>
2024-02-08 11:05:20 +0600

    initial commit

---
+++ src/Components/MainLayout/MainLayout.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const MainLayout = () => {
+  return (
+    <div className="mainLayoutContainer">
+      <h1>this is main layout</h1>
+    </div>
+  );
+};
+
+export default MainLayout;
